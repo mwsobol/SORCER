a555ac400fef477264e0cfe2c8875e26888412c3
dreedyman <dennis.reedy@gmail.com>
2014-10-30 08:58:29 -0400

    Fixing paths

--- configs/opstrings/iGridBoot.groovy
+++ configs/opstrings/iGridBoot.groovy
@@ -22,25 +22,25 @@
 import org.rioproject.RioVersion
 import sorcer.util.SorcerEnv
 
-class iGrid {
-    static String iGridHome = getIGridHome()
+class Sorcer {
+    static String sorcerHome = getSorcerHome()
     static Properties versionProps = loadVersions()
     //static String sorcerVersion = versionProps.getProperty("sorcer.version")
     static String sorcerVersion = SorcerEnv.getSorcerVersion()
     static String riverVersion = versionProps.getProperty("river.version")
     static String blitzVersion = versionProps.getProperty("blitz.version")
 
-    static getIGridHome() {
-        String iGridHome = System.getProperty("SORCER_HOME", System.getenv("SORCER_HOME"))
-        if(iGridHome==null) {
+    static getSorcerHome() {
+        String sorcerHome = System.getProperty("SORCER_HOME", System.getenv("SORCER_HOME"))
+        if(sorcerHome==null) {
             throw new RuntimeException("SORCER_HOME must be set")
         }
-        iGridHome
+        sorcerHome
     }
     
     static loadVersions() {
         Properties props = new Properties()
-        File propsFile = new File("${iGridHome}/configs/versions.properties")
+        File propsFile = new File("${sorcerHome}/configs/versions.properties")
         props.load(propsFile.newDataInputStream())
         props
     }
@@ -52,10 +52,10 @@ def appendJars(def dlJars) {
 }
 
 def getCommonDLs() {
-    return ["sorcer-dl-${iGrid.sorcerVersion}.jar",
-            "jsk-dl-${iGrid.riverVersion}.jar",
+    return ["sorcer-dl-${Sorcer.sorcerVersion}.jar",
+            "jsk-dl-${Sorcer.riverVersion}.jar",
             "rio-api-${RioVersion.VERSION}.jar",
-            "serviceui-${iGrid.riverVersion}.jar"]
+            "serviceui-${Sorcer.riverVersion}.jar"]
 }
 
 def getForkMode() {
@@ -71,98 +71,98 @@ deployment(name: "Sorcer OS") {
     service(name: SorcerEnv.getActualName('Transaction Manager')) {
         interfaces {
             classes 'net.jini.core.transaction.server.TransactionManager'
-            resources "mahalo-dl-${iGrid.riverVersion}.jar", "jsk-dl-${iGrid.riverVersion}.jar"
+            resources "mahalo-dl-${Sorcer.riverVersion}.jar", "jsk-dl-${Sorcer.riverVersion}.jar"
         }
         implementation(class: 'com.sun.jini.mahalo.TransientMahaloImpl') {
-            resources "mahalo-${iGrid.riverVersion}.jar"
+            resources "mahalo-${Sorcer.riverVersion}.jar"
         }
-        configuration new File("${iGrid.iGridHome}/bin/jini/configs/mahalo.config").text
+        configuration new File("${Sorcer.sorcerHome}/bin/jini/configs/mahalo.config").text
         maintain 1
     }
 
-    service(name: SorcerEnv.getActualSpaceName(), fork:getForkMode(), jvmArgs:"-Dsorcer.home=${iGrid.iGridHome}") {
+    service(name: SorcerEnv.getActualSpaceName(), fork:getForkMode(), jvmArgs:"-Dsorcer.home=${Sorcer.sorcerHome}") {
         interfaces {
             classes 'net.jini.space.JavaSpace05'
-            resources "blitz-dl-${iGrid.blitzVersion}.jar", "blitzui-${iGrid.blitzVersion}.jar"
+            resources "blitz-dl-${Sorcer.blitzVersion}.jar", "blitzui-${Sorcer.blitzVersion}.jar"
         }
         implementation(class: 'org.dancres.blitz.remote.BlitzServiceImpl') {
-            resources "blitz-${iGrid.blitzVersion}.jar", "blitzui-${iGrid.blitzVersion}.jar", "serviceui-${iGrid.riverVersion}.jar", "outrigger-dl-${iGrid.riverVersion}.jar"
+            resources "blitz-${Sorcer.blitzVersion}.jar", "blitzui-${Sorcer.blitzVersion}.jar", "serviceui-${Sorcer.riverVersion}.jar", "outrigger-dl-${Sorcer.riverVersion}.jar"
         }
-        configuration new File("${iGrid.iGridHome}/bin/blitz/configs/blitz.config").text
+        configuration new File("${Sorcer.sorcerHome}/bin/blitz/configs/blitz.config").text
         maintain 1
     }
 
-    service(name: SorcerEnv.getActualName("Rendezvous"), fork:getForkMode(), jvmArgs:"-Dsorcer.home=${iGrid.iGridHome}") {
+    service(name: SorcerEnv.getActualName("Rendezvous"), fork:getForkMode(), jvmArgs:"-Dsorcer.home=${Sorcer.sorcerHome}") {
         interfaces {
             classes "sorcer.core.provider.Rendezvous",
                     "sorcer.core.provider.Jobber",
                     "sorcer.core.provider.Spacer"
-            resources appendJars(["sorcer-ui-${iGrid.sorcerVersion}.jar"])
+            resources appendJars(["sorcer-ui-${Sorcer.sorcerVersion}.jar"])
         }
         implementation(class: "sorcer.core.provider.ServiceProvider") {
-            resources "rio-api-${RioVersion.VERSION}.jar"
+            resources "sorcer-lib-${Sorcer.sorcerVersion}.jar", "rio-api-${RioVersion.VERSION}.jar"
         }
-        configuration new File("${iGrid.iGridHome}/bin/sorcer/rendezvous/configs/all-rendezvous-prv.config").text
+        configuration new File("${Sorcer.sorcerHome}/bin/sorcer/rendezvous/configs/all-rendezvous-prv.config").text
         maintain 1
     }
 
     service(name: SorcerEnv.getActualName("Cataloger"), fork:getForkMode()) {
         interfaces {
             classes 'sorcer.core.provider.Cataloger'
-            resources appendJars(["sorcer-ui-${iGrid.sorcerVersion}.jar"])
+            resources appendJars(["sorcer-ui-${Sorcer.sorcerVersion}.jar"])
         }
         implementation(class: 'sorcer.core.provider.cataloger.ServiceCataloger') {
-            resources "sos-cataloger-${iGrid.sorcerVersion}.jar", "sorcer-lib-${iGrid.sorcerVersion}.jar"
+            resources "sos-cataloger-${Sorcer.sorcerVersion}.jar", "sorcer-lib-${Sorcer.sorcerVersion}.jar"
         }
-        configuration new File("${iGrid.iGridHome}/bin/sorcer/cataloger/configs/cataloger-prv.config").text
+        configuration new File("${Sorcer.sorcerHome}/bin/sorcer/cataloger/configs/cataloger-prv.config").text
         maintain 1
     }
 
     service(name: SorcerEnv.getActualName("Logger")) {
         interfaces {
             classes 'sorcer.core.provider.RemoteLogger'
-            resources appendJars(["sos-logger-${iGrid.sorcerVersion}-ui.jar", "sorcer-ui-${iGrid.sorcerVersion}.jar"])
+            resources appendJars(["sos-logger-${Sorcer.sorcerVersion}-ui.jar", "sorcer-ui-${Sorcer.sorcerVersion}.jar"])
         }
         implementation(class: 'sorcer.core.provider.logger.ServiceLogger') {
-            resources "sos-logger-${iGrid.sorcerVersion}.jar", "sorcer-lib-${iGrid.sorcerVersion}.jar"
+            resources "sos-logger-${Sorcer.sorcerVersion}.jar", "sorcer-lib-${Sorcer.sorcerVersion}.jar"
         }
-        configuration new File("${iGrid.iGridHome}/bin/sorcer/logger/configs/logger-prv.config").text
+        configuration new File("${Sorcer.sorcerHome}/bin/sorcer/logger/configs/logger-prv.config").text
         maintain 1
     }
 
-    service(name: SorcerEnv.getActualName("Exert Monitor"), fork:getForkMode(), jvmArgs:"-Dsorcer.home=${iGrid.iGridHome}") {
+    service(name: SorcerEnv.getActualName("Exert Monitor"), fork:getForkMode(), jvmArgs:"-Dsorcer.home=${Sorcer.sorcerHome}") {
         interfaces {
             classes 'sorcer.core.provider.MonitoringManagement'
-            resources appendJars(["sorcer-ui-${iGrid.sorcerVersion}.jar"])
+            resources appendJars(["sorcer-ui-${Sorcer.sorcerVersion}.jar"])
         }
         implementation(class: 'sorcer.core.provider.exertmonitor.ExertMonitor') {
-            resources "sos-exertmonitor-${iGrid.sorcerVersion}.jar", "sorcer-lib-${iGrid.sorcerVersion}.jar"
+            resources "sos-exertmonitor-${Sorcer.sorcerVersion}.jar", "sorcer-lib-${Sorcer.sorcerVersion}.jar"
         }
-        configuration new File("${iGrid.iGridHome}/bin/sorcer/exertmonitor/configs/exertmonitor-prv.config").text
+        configuration new File("${Sorcer.sorcerHome}/bin/sorcer/exertmonitor/configs/exertmonitor-prv.config").text
         maintain 1
     }
     
     service(name: SorcerEnv.getActualName("Exerter")) {
         interfaces {
             classes 'sorcer.core.provider.Exerter'
-            resources appendJars(["sorcer-ui-${iGrid.sorcerVersion}.jar"])
+            resources appendJars(["sorcer-ui-${Sorcer.sorcerVersion}.jar"])
         }
         implementation(class: 'sorcer.core.provider.ServiceTasker') {
-            resources  "sorcer-lib-${iGrid.sorcerVersion}.jar", "rio-api-${RioVersion.VERSION}.jar"
+            resources  "sorcer-lib-${Sorcer.sorcerVersion}.jar", "rio-api-${RioVersion.VERSION}.jar"
         }
-        configuration new File("${iGrid.iGridHome}/bin/sorcer/exerter/configs/exerter-prv.config").text
+        configuration new File("${Sorcer.sorcerHome}/bin/sorcer/exerter/configs/exerter-prv.config").text
         maintain 1
     }
     
-    service(name: SorcerEnv.getActualName("Database Storage"), fork:getForkMode(), jvmArgs:"-Dsorcer.home=${iGrid.iGridHome}") {
+    service(name: SorcerEnv.getActualName("Database Storage"), fork:getForkMode(), jvmArgs:"-Dsorcer.home=${Sorcer.sorcerHome}") {
         interfaces {
             classes 'sorcer.core.provider.DatabaseStorer'
-            resources appendJars(["sorcer-ui-${iGrid.sorcerVersion}.jar"])
+            resources appendJars(["sorcer-ui-${Sorcer.sorcerVersion}.jar"])
         }
         implementation(class: 'sorcer.core.provider.dbp.DatabaseProvider') {
-            resources "sos-db-prv-${iGrid.sorcerVersion}.jar", "sorcer-lib-${iGrid.sorcerVersion}.jar", "rio-api-${RioVersion.VERSION}.jar"
+            resources "sos-db-prv-${Sorcer.sorcerVersion}.jar", "sorcer-lib-${Sorcer.sorcerVersion}.jar", "rio-api-${RioVersion.VERSION}.jar"
         }
-        configuration new File("${iGrid.iGridHome}/bin/sorcer/dbp/configs/dbp-prv.config").text
+        configuration new File("${Sorcer.sorcerHome}/bin/sorcer/dbp/configs/dbp-prv.config").text
         maintain 1
     }
 }
