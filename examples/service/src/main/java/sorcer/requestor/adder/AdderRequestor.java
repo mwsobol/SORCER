b0b242ac71643eed0c85cd8c9d1b6472ce88709a
dreedyman <dennis.reedy@gmail.com>
2015-01-02 15:47:27 -0500

    Modified example to use provisioning in the

--- examples/service/src/main/java/sorcer/requestor/adder/AdderRequestor.java
+++ examples/service/src/main/java/sorcer/requestor/adder/AdderRequestor.java
@@ -1,18 +1,13 @@
 package sorcer.requestor.adder;
 
-import org.codehaus.groovy.control.CompilationFailedException;
 import sorcer.core.requestor.ServiceRequestor;
 import sorcer.provider.adder.Adder;
-import sorcer.provider.adder.impl.AdderImpl;
-import sorcer.service.ContextException;
-import sorcer.service.Exertion;
-import sorcer.service.ExertionException;
-import sorcer.service.SignatureException;
+import sorcer.service.*;
 
 import java.io.File;
 import java.io.IOException;
 
-import static sorcer.co.operator.*;
+import static sorcer.co.operator.inEnt;
 import static sorcer.eo.operator.*;
 
 public class AdderRequestor extends ServiceRequestor {
@@ -20,21 +15,20 @@ public class AdderRequestor extends ServiceRequestor {
     public Exertion getExertion(String... args)
             throws ExertionException, ContextException, SignatureException, IOException {
 
-        // by default request a local service
-        Class serviceType = AdderImpl.class;
 
         if (args[1].equals("netlet")) {
-            return (Exertion) evaluate(new File("netlets/adder-netlet.groovy"));
-        } else if (args[1].equals("remote")) {
-            serviceType =  Adder.class;
+            return (Exertion) evaluate(new File("src/main/netlets/adder-netlet.groovy"));
+        } else if (args[1].equals("dynamic")) {
+            return (Exertion) evaluate(new File("src/main/netlets/dynamic-adder-netlet.groovy"));
         }
+        Class serviceType =  Adder.class;
 
         Double v1 = new Double(getProperty("arg/x1"));
         Double v2 = new Double(getProperty("arg/x2"));
         
         return task("hello adder", sig("add", serviceType),
-                context("adder", inEnt("arg/x1", v1), inEnt("arg/x2", v2),
-                        result("out/y")));
+                    context("adder", inEnt("arg/x1", v1), inEnt("arg/x2", v2),
+                            result("out/y")));
     }
 
     @Override
