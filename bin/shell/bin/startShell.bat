ce09e7eff12b19264c952c5873756f3902ccb811
Paweł Rubach <pawel.rubach@sorcersoft.com>
2015-04-04 18:46:59 +0200

    Commented out - SorcerResolvingLoader - looks like it's not absolutely necessary

--- bin/shell/bin/startShell.bat
+++ bin/shell/bin/startShell.bat
@@ -56,7 +56,7 @@ set RIO_CLASSPATH=%RIO_CLASSPATH%;%RIO_HOME%\lib\logging\slf4j-api-%slf4j.versio
 set RIO_CLASSPATH=%RIO_CLASSPATH%;%RIO_HOME%\lib\logging\slf4j-jdk14-%slf4j.version%.jar
 
 set SORCER_PATH=
-set SORCER_PATH=%SORCER_PATH%;%LIB_DIR%\sorcer\lib\sorcer-resolving-loader-%sorcer.version%.jar
+rem set SORCER_PATH=%SORCER_PATH%;%LIB_DIR%\sorcer\lib\sorcer-resolving-loader-%sorcer.version%.jar
 set SORCER_PATH=%SORCER_PATH%;%LIB_DIR%\sorcer\lib\sorcer-platform-%sorcer.version%.jar
 set SORCER_PATH=%SORCER_PATH%;%LIB_DIR%\sorcer\lib\sos-netlet-%sorcer.version%.jar
 set SORCER_PATH=%SORCER_PATH%;%LIB_DIR%\sorcer\lib\sos-shell-%sorcer.version%.jar
@@ -83,7 +83,7 @@ set JAVA_OPTS=-Dsun.net.maxDatagramSockets=1024
 set JAVA_OPTS=%JAVA_OPTS% -Dsorcer.env.file="%SORCER_HOME%\configs\sorcer.env"
 set JAVA_OPTS=%JAVA_OPTS% -Djava.net.preferIPv4Stack=true
 set JAVA_OPTS=%JAVA_OPTS% "-Djava.protocol.handler.pkgs=net.jini.url|sorcer.util.url|org.rioproject.url"
-set JAVA_OPTS=%JAVA_OPTS% -Djava.rmi.server.RMIClassLoaderSpi=sorcer.rio.rmi.SorcerResolvingLoader
+rem set JAVA_OPTS=%JAVA_OPTS% -Djava.rmi.server.RMIClassLoaderSpi=sorcer.rio.rmi.SorcerResolvingLoader
 set JAVA_OPTS=%JAVA_OPTS% -Djava.rmi.server.useCodebaseOnly=false
 set JAVA_OPTS=%JAVA_OPTS% -Dwebster.tmp.dir="%SORCER_HOME%\data"
 set JAVA_OPTS=%JAVA_OPTS% -Dwebster.put.dir="%SORCER_HOME%\data"
