d6dafe77b37834facd66b1fb3ff2c05cb32150e2
sobolemw <sobol@sorcersoft.org>
2015-04-08 11:49:18 -0400

    updated netlets

--- examples/sml/src/main/netlets/adder.groovy
+++ examples/sml/src/main/netlets/adder.groovy
@@ -14,11 +14,9 @@
  * limitations under the License.
  */
 
-// Adder netlet for testing the requestor of the examples/server project
+codebase artifact:org.sorcer/arithmetic/jar/dl/5.2.0
 
-codebase artifact:org.sorcer/adder/jar/dl/5.2.0
-
-import sorcer.provider.adder.Adder
+import sorcer.arithmetic.provider.Adder
 
 import static sorcer.co.operator.inEnt
 import static sorcer.eo.operator.*
@@ -26,12 +24,7 @@ import static sorcer.eo.operator.*
 Double v1 = 100.0;
 Double v2 = 200.0;
 
-task("hello adder", sig("add",
-                        Adder.class,
-                        /* Dynamic deployment configuration is an artifact, and we declare that the provider
-                         * will be undeployed if idle for 1 minute */
-                        deploy(configuration("org.sorcer:adder:config:5.2.0"),
-                               idle(1))),
-     context("adder", inEnt("arg/x1", v1), inEnt("arg/x2", v2), result("out/y")));
+task("hello adder", sig("add", Adder.class),
+        context("adder", inEnt("arg/x1", v1), inEnt("arg/x2", v2), result("out/y")));
 
 
---
+++ examples/sml/src/main/netlets/f1.groovy
@@ -0,0 +1,24 @@
+codebase artifact:org.sorcer/arithmetic/jar/dl/5.2.0
+@Grab(group='org.sorcer', module='arithmetic', version='5.2.0', classifier='prv')
+
+import sorcer.arithmetic.provider.Adder
+import sorcer.arithmetic.provider.Multiplier
+import sorcer.arithmetic.provider.Subtractor
+
+
+Task f4 = task("f4",
+        sig("multiply", Multiplier.class),
+        context("multiply", inEnt("arg/x1", 10.0d), inEnt("arg/x2", 50.0d), result("result/y1")));
+
+Task f5 = task("f5",
+        sig("add", Adder.class),
+        context("add", inEnt("arg/x3", 20.0d), inEnt("arg/x4", 80.0d), result("result/y2")));
+
+Task f3 = task("f3",
+        sig("subtract", Subtractor.class),
+        context("subtract", inEnt("arg/x5"), inEnt("arg/x6"), result("result/y3")));
+
+job("f1", sig("service", Jobber.class),
+        job("f2", f4, f5), f3,
+        strategy(Provision.YES),
+        pipe(outPoint(f4, "result/y1"), inPoint(f3, "arg/x5")),
