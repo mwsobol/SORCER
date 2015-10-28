ed57aef5dc1af435424dc36ea1f980e3b0796868
sobolemw <sobol@sorcersoft.org>
2015-10-28 15:58:15 -0400

    testing multifidelities

--- examples/sml/src/test/java/sorcer/sml/contexts/MultiFidelities.java
+++ examples/sml/src/test/java/sorcer/sml/contexts/MultiFidelities.java
@@ -40,12 +40,12 @@ public class MultiFidelities {
     public void evalauteSignatureFidelityModel1() throws Exception {
 
         // three entry model
-        Model mod = operator.model(inEnt("arg/x1", 10.0), inEnt("arg/x2", 90.0),
-                ent("mFi", operator.sFi(sig("add", AdderImpl.class, operator.result("result/y", inPaths("arg/x1", "arg/x2"))),
-                        sig("multiply", MultiplierImpl.class, operator.result("result/y", inPaths("arg/x1", "arg/x2"))))),
+        Model mod = model(inEnt("arg/x1", 10.0), inEnt("arg/x2", 90.0),
+                ent("mFi", sFi(sig("add", AdderImpl.class, result("result/y", inPaths("arg/x1", "arg/x2"))),
+                        sig("multiply", MultiplierImpl.class, result("result/y", inPaths("arg/x1", "arg/x2"))))),
                 response("mFi", "arg/x1", "arg/x2"));
 
-        Context out = response(mod, operator.fi("multiply", "mFi"));
+        Context out = response(mod, fi("multiply", "mFi"));
         logger.info("out: " + out);
         assertTrue(get(out, "mFi").equals(900.0));
         assertTrue(get(mod, "result/y").equals(900.0));
@@ -55,12 +55,12 @@ public class MultiFidelities {
     public void evalauteSignaureFidelityModel2() throws Exception {
 
         // three entry model
-        Model mod = operator.model(inEnt("arg/x1", 10.0), inEnt("arg/x2", 90.0),
-                ent("mFi", operator.sFi(sig("add", AdderImpl.class, operator.result("result/y", inPaths("arg/x1", "arg/x2"))),
-                        sig("multiply", MultiplierImpl.class, operator.result("result/y", inPaths("arg/x1", "arg/x2"))))),
+        Model mod = model(inEnt("arg/x1", 10.0), inEnt("arg/x2", 90.0),
+                ent("mFi", sFi(sig("add", AdderImpl.class, result("result/y", inPaths("arg/x1", "arg/x2"))),
+                        sig("multiply", MultiplierImpl.class, result("result/y", inPaths("arg/x1", "arg/x2"))))),
                 response("mFi", "arg/x1", "arg/x2"));
 
-        Context out = response(mod, operator.fi("add", "mFi"));
+        Context out = response(mod, fi("add", "mFi"));
         logger.info("out: " + out);
         assertTrue(get(out, "mFi").equals(100.0));
         assertTrue(get(mod, "result/y").equals(100.0));
@@ -72,8 +72,8 @@ public class MultiFidelities {
         FidelityManager manager = new FidelityManager() {
             @Override
             public void initialize() {
-                Fidelity<Fidelity> fi2 = operator.fi(operator.fi("divide", "mFi2"), operator.fi("add", "mFi3"));
-                Fidelity<Fidelity> fi3 = operator.fi(operator.fi("average", "mFi2"));
+                Fidelity<Fidelity> fi2 = fi(fi("divide", "mFi2"), fi("multiply", "mFi3"));
+                Fidelity<Fidelity> fi3 = fi(fi("average", "mFi2"), fi("divide", "mFi3"));
                 put(ent("sysFi2", fi2), ent("sysFi3", fi3));
             }
 
@@ -82,7 +82,7 @@ public class MultiFidelities {
                 if (observable instanceof MultiFidelity) {
                     Fidelity<Signature> mFi = ((MultiFidelity) observable).getMultiFidelity();
                     if (mFi.getPath().equals("mFi1") && mFi.getSelectedName().equals("add")) {
-                        if (((Double) obj) >= 100.0) {
+                        if (((Double) obj) <= 200.0) {
                             morph("sysFi2");
                         } else {
                             morph("sysFi3");
@@ -93,24 +93,32 @@ public class MultiFidelities {
         };
 
         // three entry multifidelity model
-        Model mod = operator.model(inEnt("arg/x1", 90.0), inEnt("arg/x2", 10.0),
-                ent("mFi1", operator.multiFi(sig("add", AdderImpl.class, operator.result("result/y1", inPaths("arg/x1", "arg/x2"))),
-                        sig("multiply", MultiplierImpl.class, operator.result("result/y1", inPaths("arg/x1", "arg/x2"))))),
-                ent("mFi2", operator.multiFi(sig("average", AveragerImpl.class, operator.result("result/y2", inPaths("arg/x1", "arg/x2"))),
-                        sig("divide", DividerImpl.class, operator.result("result/y2", inPaths("arg/x1", "arg/x2"))),
-                        sig("subtract", SubtractorImpl.class, operator.result("result/y2", inPaths("arg/x1", "arg/x2"))))),
-                ent("mFi3", operator.multiFi(sig("average", AveragerImpl.class, operator.result("result/y3", inPaths("arg/x1", "arg/x2"))),
-                        sig("add", AdderImpl.class, operator.result("result/y3", inPaths("arg/x1", "arg/x2"))))),
+        Model mod = model(inEnt("arg/x1", 90.0), inEnt("arg/x2", 10.0),
+                ent("mFi1", multiFi(sig("add", AdderImpl.class, result("result/y1", inPaths("arg/x1", "arg/x2"))),
+                        sig("multiply", MultiplierImpl.class, result("result/y1", inPaths("arg/x1", "arg/x2"))))),
+                ent("mFi2", multiFi(sig("average", AveragerImpl.class, result("result/y2", inPaths("arg/x1", "arg/x2"))),
+                        sig("divide", DividerImpl.class, result("result/y2", inPaths("arg/x1", "arg/x2"))),
+                        sig("subtract", SubtractorImpl.class, result("result/y2", inPaths("arg/x1", "arg/x2"))))),
+                ent("mFi3", multiFi(sig("average", AveragerImpl.class, result("result/y3", inPaths("arg/x1", "arg/x2"))),
+                        sig("divide", DividerImpl.class, result("result/y2", inPaths("arg/x1", "arg/x2"))),
+                        sig("multiply", MultiplierImpl.class, result("result/y3", inPaths("arg/x1", "arg/x2"))))),
                 manager,
                 response("mFi1", "mFi2", "mFi3", "arg/x1", "arg/x2"));
 
-//        Context out = response(mod, fi("divide", "mFi1"));
+        // fidelities updated by the model's fidelity manager
         Context out = response(mod);
-
         logger.info("out: " + out);
-//        logger.info("result: " + mod.getResult());
-//        assertTrue(get(out, "mFi").equals(900.0));
-//        assertTrue(get(mod, "result/y").equals(900.0));
+        assertTrue(get(out, "mFi1").equals(100.0));
+        assertTrue(get(out, "mFi2").equals(9.0));
+        assertTrue(get(out, "mFi3").equals(900.0));
+
+        // first closing the fidelity for mFi1
+        // then fidelities updated by the model's fidelity manager accordingly
+        out = response(mod , fi("multiply", "mFi1"));
+        logger.info("out: " + out);
+        assertTrue(get(out, "mFi1").equals(900.0));
+        assertTrue(get(out, "mFi2").equals(50.0));
+        assertTrue(get(out, "mFi3").equals(9.0));
     }
 
 }
