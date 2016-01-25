b418bede10a0738177d296a4f8c27a9e659ab1a2
s13353 <s13353@pjwstk.edu.pl>
2016-01-25 21:28:17 +0100

    s13353_Tests3

--- examples/coffeemaker/src/test/java/edu/pjatk/inn/coffeemaker/tests_s13353.java
+++ examples/coffeemaker/src/test/java/edu/pjatk/inn/coffeemaker/tests_s13353.java
@@ -1,4 +1,5 @@
 package edu.pjatk.inn.coffeemaker;
+
 import edu.pjatk.inn.coffeemaker.impl.CoffeeMaker;
 import edu.pjatk.inn.coffeemaker.impl.Inventory;
 import edu.pjatk.inn.coffeemaker.impl.Recipe;
@@ -10,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.sorcer.test.ProjectContext;
 import org.sorcer.test.SorcerTestRunner;
 import sorcer.service.ContextException;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -22,12 +25,46 @@ public class tests_s13353 {
 
     private CoffeeMaker coffeeMaker;
     private Inventory inventory;
-    private Recipe espresso, mocha, macchiato, americano;
+    private Recipe Recipe;
 
     @Before
     public void setUp() throws ContextException {
         coffeeMaker = new CoffeeMaker();
         inventory = coffeeMaker.checkInventory();
+
+
+        edu.pjatk.inn.coffeemaker.impl.Recipe mocha = new Recipe();
+        mocha.setName("Mocha");
+        mocha.setPrice(60);
+        mocha.setAmtCoffee(3);
+        mocha.setAmtMilk(2);
+        mocha.setAmtSugar(2);
+        mocha.setAmtChocolate(3);
+
+        edu.pjatk.inn.coffeemaker.impl.Recipe latte = new Recipe();
+        latte.setName("Latte");
+        latte.setPrice(60);
+        latte.setAmtCoffee(3);
+        latte.setAmtMilk(3);
+        latte.setAmtSugar(2);
+        latte.setAmtChocolate(0);
+
+        edu.pjatk.inn.coffeemaker.impl.Recipe hotChocolate = new Recipe();
+        hotChocolate.setName("Hot Chocolate");
+        hotChocolate.setPrice(60);
+        hotChocolate.setAmtCoffee(0);
+        hotChocolate.setAmtMilk(2);
+        hotChocolate.setAmtSugar(2);
+        hotChocolate.setAmtChocolate(3);
+
+        edu.pjatk.inn.coffeemaker.impl.Recipe coffee = new Recipe();
+        coffee.setName("Coffee");
+        coffee.setPrice(50);
+        coffee.setAmtCoffee(3);
+        coffee.setAmtMilk(1);
+        coffee.setAmtSugar(1);
+        coffee.setAmtChocolate(0);
+
     }
 
     @Test
@@ -38,7 +75,7 @@ public class tests_s13353 {
 //	Sugar: 7
 //	Chocolate: 2
     public void addInventory1() throws Exception {
-        assertFalse(coffeeMaker.addInventory(5,3,7,2));
+        assertTrue (coffeeMaker.addInventory(5,3,7,2));
     }
 
     @Test
@@ -65,7 +102,7 @@ public class tests_s13353 {
 //	Milk: 3
 //	Sugar: -1
     public void addInventory4 () throws  Exception {
-        assertFalse(coffeeMaker.addInventory(5,3,-1,0));
+        assertFalse(coffeeMaker.addInventory(5,3,-1,0));  ////BUG should be amtSugar < 0 and cast to (int)  --- 169 line
     }
 
     @Test
