51aa1f7cf78e91be05c989539f72571b5abed5a9
Michael Sobolewski <sobol@sorcersoft.org>
2016-01-29 10:09:26 +0100

    cleanup

--- examples/coffeemaker/src/main/java/edu/pjatk/inn/requestor/CoffeemakerRequestor.java
+++ examples/coffeemaker/src/main/java/edu/pjatk/inn/requestor/CoffeemakerRequestor.java
@@ -1,313 +1,94 @@
 package edu.pjatk.inn.requestor;
 
-import edu.pjatk.inn.coffeemaker.impl.CoffeeMaker;
-import edu.pjatk.inn.coffeemaker.impl.Inventory;
-import edu.pjatk.inn.coffeemaker.impl.Recipe;
+import edu.pjatk.inn.coffeemaker.CoffeeService;
+import edu.pjatk.inn.coffeemaker.Delivery;
+import sorcer.core.requestor.ExertRequestor;
+import sorcer.service.*;
+import sorcer.service.modeling.Model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.File;
 
-/**
- * 
- * @author Sarah E. Smith & Mike Sobolewski
- *
- * Starts the console UI for the CoffeeMaker
- */
-public class CoffeemakerRequestor {
-    private static CoffeeMaker coffeeMaker;
+import static sorcer.co.operator.*;
+import static sorcer.eo.operator.*;
+import static sorcer.mo.operator.responseUp;
+import static sorcer.po.operator.invoker;
 
-    public static void mainMenu() {
-        System.out.println("1. Add a recipe");
-        System.out.println("2. Delete a recipe");
-        System.out.println("3. Edit a recipe");
-        System.out.println("4. Add inventory");
-        System.out.println("5. Check inventory");
-        System.out.println("6. Make coffee");
-        System.out.println("0. Exit\n");
-        
-        //Get user input
-        int userInput = stringToInt(inputOutput("Please press the number that corresponds to what you would like the coffee maker to do."));
-        
-        if(userInput == 1) addRecipe();
-        if(userInput == 2) deleteRecipe();
-        if(userInput == 3) editRecipe();
-        if(userInput == 4) addInventory();
-        if(userInput == 5) checkInventory();
-        if(userInput == 6) makeCoffee();
-        if(userInput == 0) System.exit(0);
-    }
-	public static void addRecipe() {
-	    //Read in recipe name
-	    String name = inputOutput("\nPlease enter the recipe name: ");
-	    
-	    //Read in recipe price
-	    String priceString = inputOutput("\nPlease enter the recipe price: $");
-	    int price = stringToInt(priceString);
-	    if(price < 0) {
-	    	System.out.println(name + " could not be added. Price can not be negative");
-	    	mainMenu();
-	    }
-	    
-	    //Read in amt coffee
-	    String coffeeString = inputOutput("\nPlease enter the units of coffee in the recipe: ");
-	    int amtCoffee = stringToInt(coffeeString);
-	    if(amtCoffee < 0) {
-	    	System.out.println(name + " could not be added. Units of coffee can not be negative");
-	    	mainMenu();
-	    }
-	    
-	    //Read in amt milk
-	    String milkString = inputOutput("\nPlease enter the units of milk in the recipe: ");
-	    int amtMilk = stringToInt(milkString);
-	    if(amtMilk < 0) {
-	    	System.out.println(name + " could not be added. Units of milk can not be negative");
-	    	mainMenu();
-	    }
-	    
-	    //Read in amt sugar
-	    String sugarString = inputOutput("\nPlease enter the units of sugar in the recipe: ");
-	    int amtSugar = stringToInt(sugarString);
-	    if(amtSugar < 0) {
-	    	System.out.println(name + " could not be added. Units of sugar can not be negative");
-	    	mainMenu();
-	    }
-	    
-	    //Read in amt chocolate
-	    String chocolateString = inputOutput("\nPlease enter the units of chocolate in the recipe: ");
-	    int amtChocolate = stringToInt(chocolateString);
-	    if(amtChocolate < 0) {
-	    	System.out.println(name + " could not be added. Units of chocolate can not be negative");
-	    	mainMenu();
-	    }
-	    
-	    boolean recipeAdded = false;
-		Recipe r = new Recipe();
-		r.setName(name);
-		r.setPrice(price);
-		r.setAmtCoffee(amtCoffee);
-		r.setAmtMilk(amtMilk);
-		r.setAmtSugar(amtSugar);
-		r.setAmtChocolate(amtChocolate);
-		   
-		recipeAdded = coffeeMaker.addRecipe(r);
-	    
-	    if(recipeAdded) System.out.println(name + " successfully added.\n");
-	    else System.out.println(name + " could not be added.\n");
-	    
-	    mainMenu();
-    }
-    
-    public static void deleteRecipe() {
-        Recipe [] recipes = coffeeMaker.getRecipes();
-        boolean [] recipeFull = coffeeMaker.getRecipeFull();
-        boolean recipe = false;
-        for(int i = 0; i < recipeFull.length; i++) {
-        	if(recipeFull[i]) {
-        		recipe = true;
-        	}
+public class CoffeemakerRequestor extends ExertRequestor {
+
+    public Mogram getMogram(String... args) throws MogramException {
+
+        String option = "exertion";
+        if (args != null && args.length == 2) {
+            option = args[1];
+        } else if (this.args != null) {
+            option = this.args[0];
+        } else {
+            throw new MogramException("wrong arguments for: ExertRequestor type, mogram type");
         }
-        if(recipe) {
-	        for(int i = 0; i < recipes.length; i++) {
-	            System.out.println((i+1) + ". " + recipes[i].getName());
-	        }
-	        String recipeToDeleteString = inputOutput("Please select the number of the recipe to delete.");
-	        int recipeToDelete = stringToInt(recipeToDeleteString) - 1;
-		    if(recipeToDelete < 0) {
-		    	mainMenu();
-		    }
-	        String name = recipes[recipeToDelete].getName();
-	        boolean recipeDeleted = coffeeMaker.deleteRecipe(recipes[recipeToDelete]);
-	        
-	        if(recipeDeleted) System.out.println(name + " successfully deleted.\n");
-		    else System.out.println(name + "could not be deleted.\n");
-        }
-        else {
-        	System.out.println("There are no getRecipes to delete\n");
-        }
-        mainMenu();
-    }
-    
-    public static void editRecipe() {
-        Recipe [] recipes = coffeeMaker.getRecipes();
-        boolean [] recipeFull = coffeeMaker.getRecipeFull();
-        boolean recipe = false;
-        for(int i = 0; i < recipeFull.length; i++) {
-        	if(recipeFull[i]) {
-        		recipe = true;
-        	}
-        }
-        if(recipe) {
-	        for(int i = 0; i < recipes.length; i++) {
-	            System.out.println((i+1) + ". " + recipes[i].getName());
-	        }
-	        String recipeToEditString = inputOutput("Please select the number of the recipe to edit.");
-	        int recipeToEdit = stringToInt(recipeToEditString) -1;
-		    if(recipeToEdit < 0) {
-		    	mainMenu();
-		    }
-	        
-	        Recipe oldRecipe = recipes[recipeToEdit];
-	        
-		    //Read in recipe name
-		    String name = inputOutput("\nPlease enter the recipe name: ");
-		    
-		    //Read in recipe price
-		    String priceString = inputOutput("\nPlease enter the recipe price: $");
-		    int price = stringToInt(priceString);
-		    if(price < 0) {
-		    	System.out.println(name + " could not be edited. Price can not be negative");
-		    	mainMenu();
-		    }
-		    
-		    //Read in amt coffee
-		    String coffeeString = inputOutput("\nPlease enter the units of coffee in the recipe: ");
-		    int amtCoffee = stringToInt(coffeeString);
-		    if(amtCoffee < 0) {
-		    	System.out.println(name + " could not be edited. Units of coffee can not be negative");
-		    	mainMenu();
-		    }
-		    
-		    //Read in amt milk
-		    String milkString = inputOutput("\nPlease enter the units of milk in the recipe: ");
-		    int amtMilk = stringToInt(milkString);
-		    if(amtMilk < 0) {
-		    	System.out.println(name + " could not be edited. Units of milk can not be negative");
-		    	mainMenu();
-		    }
-		    
-		    //Read in amt sugar
-		    String sugarString = inputOutput("\nPlease enter the units of sugar in the recipe: ");
-		    int amtSugar = stringToInt(sugarString);
-		    if(amtSugar < 0) {
-		    	System.out.println(name + " could not be edited. Units of sugar can not be negative");
-		    	mainMenu();
-		    }
-		    
-		    //Read in amt chocolate
-		    String chocolateString = inputOutput("\nPlease enter the units of chocolate in the recipe: ");
-		    int amtChocolate = stringToInt(chocolateString);
-		    if(amtChocolate < 0) {
-		    	System.out.println(name + " could not be edited. Units of chocolate can not be negative");
-		    	mainMenu();
-		    }
-		    
-		    Recipe newRecipe = new Recipe();
-		    newRecipe.setName(name);
-		    newRecipe.setPrice(price);
-		    newRecipe.setAmtCoffee(amtCoffee);
-		    newRecipe.setAmtMilk(amtMilk);
-		    newRecipe.setAmtSugar(amtSugar);
-		    newRecipe.setAmtChocolate(amtChocolate);
-	        
-	        boolean recipeEdited = coffeeMaker.editRecipe(oldRecipe, newRecipe);
-	        
-	        if(recipeEdited) System.out.println(oldRecipe.getName() + " successfully edited.\n");
-		    else System.out.println(oldRecipe.getName() + "could not be edited.\n");
-        }
-        else {
-        	System.out.println("There are no getRecipes to edit.\n");
-        }
-        mainMenu();
-    }
-    
-    public static void addInventory() {
-	    //Read in amt coffee
-	    String coffeeString = inputOutput("\nPlease enter the units of coffee to add: ");
-	    int amtCoffee = stringToInt(coffeeString);
-	    if(amtCoffee < 0) {
-	    	System.out.println("Cannot add inventory. Units of coffee can not be negative");
-	    	mainMenu();
-	    }
-	    
-	    //Read in amt milk
-	    String milkString = inputOutput("\nPlease enter the units of milk to add: ");
-	    int amtMilk = stringToInt(milkString);
-	    if(amtMilk < 0) {
-	    	System.out.println("Cannot add inventory. Units of milk can not be negative");
-	    	mainMenu();
-	    }
-	    
-	    //Read in amt sugar
-	    String sugarString = inputOutput("\nPlease enter the units of sugar to add: ");
-	    int amtSugar = stringToInt(sugarString);
-	    if(amtSugar < 0) {
-	    	System.out.println("Cannot add inventory. Units of sugar can not be negative");
-	    	mainMenu();
-	    }
-	    
-	    //Read in amt chocolate
-	    String chocolateString = inputOutput("\nPlease enter the units of chocolate to add: ");
-	    int amtChocolate = stringToInt(chocolateString);
-	    if(amtChocolate < 0) {
-	    	System.out.println("Cannot add inventory. Units of chocolate can not be negative");
-	    	mainMenu();
-	    }
-	    
-        boolean added = coffeeMaker.addInventory(amtCoffee, amtMilk, amtSugar, amtChocolate);
-        if(added) System.out.println("Inventory successfully added");
-        else System.out.println("Inventory was not added");
-        mainMenu();
-    }
-    
-    public static void checkInventory() {
-        Inventory inventory = coffeeMaker.checkInventory();
-        System.out.println(inventory.toString());
-        mainMenu();
-    }
-    
-    public static void makeCoffee() {
-        Recipe [] recipes = coffeeMaker.getRecipes();
-        for(int i = 0; i < recipes.length; i++) {
-            System.out.println((i+1) + ". " + recipes[i].getName());
-        }
-        String recipeToPurchaseString = inputOutput("Please select the number of the recipe to purchase.");
-        int recipeToPurchase = stringToInt(recipeToPurchaseString) -1;
-	    if(recipeToPurchase < 0) {
-	    	mainMenu();
-	    }
-        
-        String amountPaid = inputOutput("Please enter the amount you wish to pay");
-        int amountToPay = stringToInt(amountPaid);
-	    if(amountToPay < 0) {
-	    	mainMenu();
-	    }
-        
-        Recipe recipe = recipes[recipeToPurchase];
-        int change = coffeeMaker.makeCoffee(recipe, amountToPay);
-        
-        System.out.println("Your change is: " + change + "\n");
-        mainMenu();
-    }
-    
-    public static String inputOutput(String message) {
-        System.out.println(message);
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    String returnString = "";
-	    try {
-	        returnString = br.readLine();
-	    }
-	    catch (IOException e){
-	        System.out.println("Error reading in value");
-	        mainMenu();
-	    }
-	    return returnString;
-    }
-    
-    private static int stringToInt(String value) {
-        int returnInt = -1;
         try {
-            returnInt = Integer.parseInt(value);
+            if (option.equals("netlet")) {
+                return (Exertion) evaluate(new File("src/main/netlets/coffeemaker-exertion-remote.ntl"));
+            } else if (option.equals("model")) {
+                return createModel();
+            } else if (option.equals("exertion")) {
+                return createExertion();
+            }
+        } catch (Exception e) {
+            throw new MogramException(e);
         }
-        catch (NumberFormatException e) {
-            System.out.println("Please input an integer\n");
-        }
-        return returnInt;
+        return null;
     }
-    
-    public static void main(String[] args) {
-	    coffeeMaker = new CoffeeMaker();
-	    System.out.println("Welcome to the CoffeeMaker!\n");
-	    mainMenu();
-	}
+
+    private Context getEspressoContext() throws ContextException {
+        return context(ent("name", "espresso"), ent("price", 50),
+                ent("amtCoffee", 6), ent("amtMilk", 0),
+                ent("amtSugar", 1), ent("amtChocolate", 0));
+    }
+
+    private Task getRecipeTask() throws MogramException, SignatureException {
+        // make sure we have a recipe for required coffee
+       return task("recipe", sig("addRecipe", CoffeeService.class), getEspressoContext());
+    }
+
+    private Exertion createExertion() throws Exception {
+        Task coffee = task("coffee", sig("makeCoffee", CoffeeService.class), context(
+                ent("recipe/name", "espresso"),
+                ent("coffee/paid", 120),
+                ent("coffee/change"),
+                ent("recipe", getEspressoContext())));
+
+        Task delivery = task("delivery", sig("deliver", Delivery.class), context(
+                ent("location", "PJATK"),
+                ent("delivery/paid"),
+                ent("room", "101")));
+
+        Job drinkCoffee = job(coffee, delivery,
+                pipe(outPoint(coffee, "coffee/change"), inPoint(delivery, "delivery/paid")));
+
+        return drinkCoffee;
+    }
+
+    private Model createModel() throws Exception {
+        exert(getRecipeTask());
+
+        // order espresso with delivery
+        Model mdl = model(
+                ent("recipe/name", "espresso"),
+                ent("paid$", 120),
+                ent("location", "PJATK"),
+                ent("room", "101"),
+
+                srv(sig("makeCoffee", CoffeeService.class,
+                        result("coffee$", inPaths("recipe/name")))),
+                srv(sig("deliver", Delivery.class,
+                        result("delivery$", inPaths("location", "room")))));
+//				ent("change$", invoker("paid$ - (coffee$ + delivery$)", ents("paid$", "coffee$", "delivery$"))));
+
+        add(mdl, ent("change$", invoker("paid$ - (coffee$ + delivery$)", ents("paid$", "coffee$", "delivery$"))));
+        dependsOn(mdl, ent("change$", "makeCoffee"), ent("change$", "deliver"));
+        responseUp(mdl, "makeCoffee", "deliver", "change$", "paid$");
+
+        return mdl;
+    }
+
 }
