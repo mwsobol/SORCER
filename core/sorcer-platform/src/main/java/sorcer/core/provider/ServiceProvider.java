e6ef21d38f8ecc853d7d6b669d53bb2d7b8e3247
burtonsa <scottaburton@amopti.com>
2016-05-12 12:37:43 -0400

    rolled version

--- core/sorcer-platform/src/main/java/sorcer/core/provider/ServiceProvider.java
+++ core/sorcer-platform/src/main/java/sorcer/core/provider/ServiceProvider.java
@@ -1262,11 +1262,12 @@ public class ServiceProvider implements Identifiable, Provider, ServiceIDListene
 			e.printStackTrace();
 		}
 		String msg = "host = " + host + " "
-				   + "\ntotal service op calls = " + numCalls + " "
-				   + "\nnumber of service op calls running = "	+ numThreads + " "
+				   + "\ntotal service op calls      = " + numCalls + " "
+				   + "\nservice op calls running    = "	+ numThreads + " "
 				   + "\nservice op call ids running = " + threadIds + " "
-		           + "\naverage exec time = " + avgExecTime;
-		return msg;
+		           + "\naverage exec time           = " + avgExecTime;
+
+        return msg;
 	}
 
 	public synchronized String doThreadMonitor(String serviceIdString) {
