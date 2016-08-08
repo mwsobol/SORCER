95e41a3fef98bf88592f109da1fce82fd706d0ab
sobolemw <sobol@sorcersoft.org>
2016-08-08 19:24:24 -0400

    testing cleanup

--- core/sorcer-platform/src/main/java/sorcer/service/ServiceMogram.java
+++ core/sorcer-platform/src/main/java/sorcer/service/ServiceMogram.java
@@ -612,6 +612,8 @@ public abstract class ServiceMogram implements Mogram, Exec, Serializable, Sorce
     public boolean isSuper() {
         return isSuper;
     }
+git status
+
 
     public void setSuper(boolean aSuper) {
         isSuper = aSuper;
