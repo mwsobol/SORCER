e74dfae4d142fdd23e3dc1ae4c151953b4611c83
sobolemw <sobol@sorcersoft.org>
2017-09-21 18:09:48 -0400

    cleanup class hierarchy semantics

--- core/sorcer-dl/src/main/java/sorcer/core/provider/Provider.java
+++ core/sorcer-dl/src/main/java/sorcer/core/provider/Provider.java
@@ -40,7 +40,7 @@ import java.util.Properties;
  * @see Monitorable
  * @see Remote
  */
-public interface Provider extends Activity,  Exerter, Monitorable, AnalyticsProducer, Administrable, Remote {
+public interface Provider extends Exerter, Monitorable, AnalyticsProducer, Administrable, Remote {
 
 	public ServiceID getProviderID() throws RemoteException;
 
