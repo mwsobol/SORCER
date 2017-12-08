832b5a2b7f7db9f5d2e51cd78f0c7ec953ea88ba
dreedyman <dennis.reedy@gmail.com>
2017-12-08 14:15:11 -0500

    Update rio

--- core/sorcer-int-tests/deploy-tests/src/test/java/sorcer/core/deploy/OperationalStringFactoryTest.java
+++ core/sorcer-int-tests/deploy-tests/src/test/java/sorcer/core/deploy/OperationalStringFactoryTest.java
@@ -25,8 +25,6 @@ import org.rioproject.system.capability.connectivity.TCPConnectivity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sorcer.co.operator;
-import sorcer.core.provider.ProviderName;
-import sorcer.core.signature.ServiceSignature;
 import sorcer.service.*;
 import sorcer.util.SorcerEnv;
 
